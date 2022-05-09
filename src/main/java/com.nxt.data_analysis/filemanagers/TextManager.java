5e9491adbdd50ed68fd835355fed5f9e731dd8d6
decagon <godsonjonathan39@gmail.com>
2022-05-09 11:00:23 +0100

    Implemented TextManager

--- src/main/java/com.nxt.data_analysis/filemanagers/TextManager.java
+++ src/main/java/com.nxt.data_analysis/filemanagers/TextManager.java
@@ -1,4 +1,43 @@
 package com.nxt.data_analysis.filemanagers;
 
-public class TextManager {
+import com.nxt.data_analysis.interfaces.FileManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TextManager implements FileManager {
+
+    private final String  fileExtension = ".txt";
+
+    @Override
+    public String getFileExtension() {
+        return fileExtension;
+    }
+
+    @Override
+    public void moveFile(Path path, Path destination) {
+        try {
+            Files.move(path, destination.resolve(path.getFileName().toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String parseFile(Path path) {
+        StringBuilder fileContent = new StringBuilder();
+        try {
+            BufferedReader reader = Files.newBufferedReader(path);
+            String line;
+            while ((line = reader.readLine()) != null){
+                fileContent.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent.toString();
+    }
 }
