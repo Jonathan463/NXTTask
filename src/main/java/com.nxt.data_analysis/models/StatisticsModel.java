fa1873a6ca88a77858bea6d7dd715b23cc998f08
decagon <godsonjonathan39@gmail.com>
2022-05-09 11:07:53 +0100

    StatisticsModel Implementation

--- src/main/java/com.nxt.data_analysis/models/StatisticsModel.java
+++ src/main/java/com.nxt.data_analysis/models/StatisticsModel.java
@@ -1,4 +1,46 @@
 package com.nxt.data_analysis.models;
 
 public class StatisticsModel {
+    private String nameOfStat;
+    private String valueOfStat;
+    private String nameOfFile;
+    public StatisticsModel() {
+    }
+
+    public StatisticsModel(String nameOfStat, String valueOfStat) {
+        this.nameOfStat = nameOfStat;
+        this.valueOfStat = valueOfStat;
+    }
+
+    public String getNameOfStat() {
+        return nameOfStat;
+    }
+
+    public void setNameOfStat(String nameOfStat) {
+        this.nameOfStat = nameOfStat;
+    }
+
+    public String getValueOfStat() {
+        return valueOfStat;
+    }
+
+    public void setValueOfStat(String valueOfStat) {
+        this.valueOfStat = valueOfStat;
+    }
+
+    public String getNameOfFile() {
+        return nameOfFile;
+    }
+
+    public void setNameOfFile(String nameOfFile) {
+        this.nameOfFile = nameOfFile;
+    }
+
+    public void logStatNameAndValue(){
+        System.out.println("Name of file  " + getNameOfFile());
+        System.out.println(getNameOfStat() + "   ===   " + getValueOfStat());
+        System.out.println("=================================================>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("=================================================>>>>>>>>>>>>>>>>>>>>>");
+
+    }
 }
